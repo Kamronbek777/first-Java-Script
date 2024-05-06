29e74723e86f629f703f0bbfe27b39be03a48009
Kamronbek777 <kamronbek25082010@gmail.com>
2024-05-06 21:19:55 +0500

    first Java Script code

---
+++ script.js
@@ -0,0 +1,8 @@
+let userName = prompt("Enter your name");
+let userSurname = prompt("Enter your surname");
+let userAge = +prompt("Enter your age");
+let userAddress = prompt("Enter your address");
+let userPassword = prompt("Enter your password");
+let message = alert("Thank you!");
+
+console.log("Name:", userName + "\n" + "Surname:", userSurname  + "\n" +  "Age:", userAge  + "\n" +  "Address:", userAddress  + "\n" +  "Password:", userPassword, message);
